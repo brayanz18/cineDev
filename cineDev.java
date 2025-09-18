4341ea7f5e1d032b4b4c810ed92b754c8c4e335b
brayanz18 <gabrielbrayan994@gmail.com>
2025-09-18 17:16:41 -0300

    ultimo commit

--- cineDev.java
+++ cineDev.java
@@ -43,6 +43,7 @@ public class cineDev {
         System.out.println("2.Comprar ingresso");
         System.out.println("3.Exibir relatório de ocupação");
         System.out.println("4.Sair");
+        System.out.println("5.Créditos dos criadores");
         opcao = sc.nextInt();//define a opção que o usuário vai selecionar no sistema
         //opção 1
         if (opcao == 1){
@@ -57,7 +58,7 @@ public class cineDev {
         } else if(opcao == 2){
             System.out.println("Digite o numero da fileira 0 a 9");
             int fileira = sc.nextInt();
-            System.out.println("Digite o numero do assento");
+            System.out.println("Digite o numero do assento 0 a 19");
             int assento = sc.nextInt();
             System.out.println("Confirme a compra (se você cometeu um engano e deseja cancelar !!NÃO CONFIRME A COMPRA!!)");    
             String confirma = sc.next();
@@ -71,8 +72,36 @@ public class cineDev {
             }else {
                 System.out.println("Compra cancelada");
             }
+            //opção 3
+        } else if (opcao == 3){
+            //definindo variáveis para os assentos ocupados e o total de assentos 
+            int total = 200;
+            int ocupados = 0;
+            //comparando os ocupados com o total de assentos
+            for (int[] linha : acentos){
+                for (int assentos : linha ){
+                    if (assentos == 0){
+                        ocupados++;
+                    }
+                }
+            }   
+            //definindo a média de assentos e a porcentagem.
+        int livres = total - ocupados;
+        double porcentagem = (ocupados *100) / total;
+        System.out.println("=== RELATÓRIO DE OCUPAÇÃO ===");
+        System.out.println("Total de assentos: " + total);
+        System.out.println("Assentos ocupados: " + ocupados);
+        System.out.println("Assentos livres: " + livres);
+        System.out.printf("A porcentagem de acentos ocupados ; %.2f",porcentagem);
+        }
+            //A opção 5 está  abaixo da 4 por que os créditos foram colocados de ultima hora com um tempo restante que havia em aula.
+        else if (opcao == 5){
+            System.out.println("Criação e Desenvolvimento: "
+                    + "Brayan Gabriel Bitencourt de Oliveira"
+                    + "Vitor da Silva Coelho");
         }
         }while(opcao != 4);
+            System.out.println("Você saiu do sistema");
         }
     }
 }
